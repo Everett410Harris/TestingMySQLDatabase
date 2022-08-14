b84685b0091348f0d9872c4d9231c550bf2190bf
Everett Harris <everettharr979@gmail.com>
2022-08-14 17:12:35 -0400

    N/A

--- TestingMySQLDatabase/Form1.cs
+++ TestingMySQLDatabase/Form1.cs
@@ -12,7 +12,7 @@ namespace TestingMySQLDatabase
         private void loadDataBtn_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            string ConnectionString = "sever=localhost;user=root;database=world;port=3306;password=DREADhead2638!";
+            string ConnectionString = "server=localhost;user=root;database=world;port=3306;password=DREADhead2638!";
             MySqlConnection con = new MySqlConnection(ConnectionString);
             con.Open();
             MySqlCommand cmd = new MySqlCommand("SELECT Name FROM country;", con);
