32f8169105efc4213f2b40f45acc73269b44083a
Everett Harris <everettharr979@gmail.com>
2022-08-14 16:58:07 -0400

    added mysql package, created button for connection, listbox to display information

--- TestingMySQLDatabase/Form1.Designer.cs
+++ TestingMySQLDatabase/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.loadDataBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 30;
+            this.listBox1.Location = new System.Drawing.Point(152, 373);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(612, 274);
+            this.listBox1.TabIndex = 0;
+            // 
+            // loadDataBtn
+            // 
+            this.loadDataBtn.Location = new System.Drawing.Point(274, 122);
+            this.loadDataBtn.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.loadDataBtn.Name = "loadDataBtn";
+            this.loadDataBtn.Size = new System.Drawing.Size(421, 46);
+            this.loadDataBtn.TabIndex = 1;
+            this.loadDataBtn.Text = "LOAD DATA";
+            this.loadDataBtn.UseVisualStyleBackColor = true;
+            this.loadDataBtn.Click += new System.EventHandler(this.loadDataBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(964, 702);
+            this.Controls.Add(this.loadDataBtn);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Button loadDataBtn;
     }
 }
--- TestingMySQLDatabase/Form1.cs
+++ TestingMySQLDatabase/Form1.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 namespace TestingMySQLDatabase
 {
     public partial class Form1 : Form
@@ -5,6 +7,23 @@ namespace TestingMySQLDatabase
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadDataBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string ConnectionString = "sever=localhost;user=root;database=world;port=3306;password=DREADhead2638!";
+            MySqlConnection con = new MySqlConnection(ConnectionString);
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT Name FROM country;", con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader.GetString(0));
+            }
+            reader.Close();
+            cmd.Dispose();
+            con.Close();
         }
     }
 }
